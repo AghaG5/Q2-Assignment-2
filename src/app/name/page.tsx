f9cbacac373af34baab77b881d27ab97ee440479
Mussa Ali Haider <musaalihaider5@gmail.com>
2024-08-10 03:21:23 +0500

    assignment 2

---
+++ src/app/name/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link"
+export default function name(){
+  return(
+    <div>
+        <h1>This is name page</h1>
+        <br />
+        <Link href = "/">Home page </Link>
+    </div>
+  )  
+}
